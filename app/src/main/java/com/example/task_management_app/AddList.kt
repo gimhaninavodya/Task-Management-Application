b264f80c592f75d266f67cdac7bab695526bf43c
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2024-05-07 11:13:13 +0530

    activity listMain and addList added.

---
+++ app/src/main/java/com/example/task_management_app/AddList.kt
@@ -0,0 +1,11 @@
+package com.example.task_management_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_list)
+    }
+}

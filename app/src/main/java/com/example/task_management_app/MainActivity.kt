785069381bb71f692e36ee8dcbfbdbeec2fe63f9
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2024-05-07 13:05:40 +0530

    set the activities path and added styles.

--- app/src/main/java/com/example/task_management_app/MainActivity.kt
+++ app/src/main/java/com/example/task_management_app/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.task_management_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBg : ImageView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mainBg.setOnClickListener{
+            val intent = Intent(this@MainActivity,ListMain::class.java )
+            startActivity(intent)
+        }
     }
 }
